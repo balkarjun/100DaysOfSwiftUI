8c003820c4852629b5f3ffd1ae50a2593d7367ac
Arjun Balakrishnan <balkarjun@gmail.com>
2022-08-27 18:58:47 +0530

    add files

---
+++ P01-WeSplit/P01-WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P01-WeSplit
+//
+//  Created by Arjun B on 27/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ P01-WeSplit/P01-WeSplit/P01_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  P01_WeSplitApp.swift
+//  P01-WeSplit
+//
+//  Created by Arjun B on 27/08/22.
+//
+
+import SwiftUI
+
+@main
+struct P01_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
