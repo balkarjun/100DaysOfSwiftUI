a9980f3868260fafacaacf88c8c65081755324fe
Arjun Balakrishnan <balkarjun@gmail.com>
2022-08-27 21:51:24 +0530

    add files

---
+++ C01-Convert/C01-Convert/C01_ConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  C01_ConvertApp.swift
+//  C01-Convert
+//
+//  Created by Arjun B on 27/08/22.
+//
+
+import SwiftUI
+
+@main
+struct C01_ConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
