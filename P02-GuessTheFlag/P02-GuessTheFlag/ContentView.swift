c2e4489085f01e0697996874ae0d5545883975cc
Arjun Balakrishnan <balkarjun@gmail.com>
2022-08-28 16:48:31 +0530

    add game logic

--- P02-GuessTheFlag/P02-GuessTheFlag/ContentView.swift
+++ P02-GuessTheFlag/P02-GuessTheFlag/ContentView.swift
@@ -8,9 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
     
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var score = 0
     
     var body: some View {
         ZStack {
@@ -25,7 +29,14 @@ struct ContentView: View {
                 
                 ForEach(0..<3) { number in
                     Button {
-                        // flag
+                        if number == correctAnswer {
+                            scoreTitle = "Correct"
+                            score += 1
+                        } else {
+                            scoreTitle = "Wrong"
+                        }
+                        
+                        showingScore = true
                     } label: {
                         Image(countries[number])
                             .renderingMode(.original)
@@ -33,6 +44,16 @@ struct ContentView: View {
                 }
             }
         }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is \(score)")
+        }
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
