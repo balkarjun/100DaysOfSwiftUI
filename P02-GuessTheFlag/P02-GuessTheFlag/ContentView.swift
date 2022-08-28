bfd9cf472132705913273992a0a1d915ae9f3489
Arjun Balakrishnan <balkarjun@gmail.com>
2022-08-28 13:12:07 +0530

    init project

---
+++ P02-GuessTheFlag/P02-GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P02-GuessTheFlag
+//
+//  Created by Arjun B on 28/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ P02-GuessTheFlag/P02-GuessTheFlag/P02_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  P02_GuessTheFlagApp.swift
+//  P02-GuessTheFlag
+//
+//  Created by Arjun B on 28/08/22.
+//
+
+import SwiftUI
+
+@main
+struct P02_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
