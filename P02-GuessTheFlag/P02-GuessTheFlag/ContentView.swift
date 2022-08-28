539760576f212f0bbd0bf77a39ae5eefea2ce737
Arjun Balakrishnan <balkarjun@gmail.com>
2022-08-28 23:09:03 +0530

    update ui

--- P02-GuessTheFlag/P02-GuessTheFlag/ContentView.swift
+++ P02-GuessTheFlag/P02-GuessTheFlag/ContentView.swift
@@ -15,34 +15,97 @@ struct ContentView: View {
     @State private var correctAnswer = Int.random(in: 0...2)
     
     @State private var score = 0
+    @State private var total = 0
     
     var body: some View {
         ZStack {
-            Color.secondary
-                .ignoresSafeArea()
-            
             VStack(spacing: 30) {
                 VStack {
-                    Text("Tap the flag of")
+                    Text("GUESS THE FLAG OF")
+                        .bold()
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+
                     Text(countries[correctAnswer])
+                        .bold()
+                        .font(.largeTitle)
                 }
                 
-                ForEach(0..<3) { number in
-                    Button {
-                        if number == correctAnswer {
-                            scoreTitle = "Correct"
-                            score += 1
-                        } else {
-                            scoreTitle = "Wrong"
+                VStack(spacing: 20) {
+                    ForEach(0..<3) { number in
+                        Button {
+                            if number == correctAnswer {
+                                scoreTitle = "Correct"
+                                score += 1
+                            } else {
+                                scoreTitle = "Wrong"
+                            }
+                            
+                            total += 1
+                            showingScore = true
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .clipShape(RoundedRectangle(cornerRadius: 8))
+                                .shadow(radius: 10)
                         }
-                        
-                        showingScore = true
-                    } label: {
-                        Image(countries[number])
-                            .renderingMode(.original)
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 30)
+                .background(.thinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 16))
+                
+                HStack {
+                    Spacer()
+                    
+                    VStack {
+                        Text("CORRECT")
+                            .bold()
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                        
+                        if total != 0 {
+                            Text("\(score)")
+                                .bold()
+                                .font(.title2)
+                                .foregroundColor(.green)
+                        } else {
+                            Text("——")
+                                .bold()
+                                .opacity(0.5)
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                    .frame(minHeight: 60)
+                    
+                    Spacer()
+                    Spacer()
+                    
+                    VStack {
+                        Text("WRONG")
+                            .bold()
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                        
+                        if total != 0 {
+                            Text(total - score, format: .number)
+                                .bold()
+                                .font(.title2)
+                                .foregroundColor(.red)
+                        } else {
+                            Text("——")
+                                .bold()
+                                .opacity(0.5)
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                    .frame(minHeight: 60)
+                    
+                    Spacer()
+                }
             }
+            .padding(.horizontal, 30)
         }
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
