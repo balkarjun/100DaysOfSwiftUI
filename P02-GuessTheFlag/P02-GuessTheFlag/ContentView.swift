d49637aa670e9145aeae44064eafaebef860b008
Arjun Balakrishnan <balkarjun@gmail.com>
2022-08-28 16:39:00 +0530

    add assets

--- P02-GuessTheFlag/P02-GuessTheFlag/ContentView.swift
+++ P02-GuessTheFlag/P02-GuessTheFlag/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    
+    var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.secondary
+                .ignoresSafeArea()
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                    Text(countries[correctAnswer])
+                }
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        // flag
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+            }
+        }
     }
 }
 
