bb6bd1ea61aa9a4d2b4d54c13a19e6ffc066b73e
Arjun Balakrishnan <balkarjun@gmail.com>
2022-08-31 18:04:14 +0530

    add new project

---
+++ P03-ViewsAndModifiers/P03-ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P03-ViewsAndModifiers
+//
+//  Created by Arjun B on 31/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ P03-ViewsAndModifiers/P03-ViewsAndModifiers/P03_ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  P03_ViewsAndModifiersApp.swift
+//  P03-ViewsAndModifiers
+//
+//  Created by Arjun B on 31/08/22.
+//
+
+import SwiftUI
+
+@main
+struct P03_ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
