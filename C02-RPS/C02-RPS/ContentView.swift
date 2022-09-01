89e0c22388ec0a0419fd57d6d630a5b3b2b79853
Arjun Balakrishnan <balkarjun@gmail.com>
2022-09-01 18:29:33 +0530

    add basic logic

--- C02-RPS/C02-RPS/ContentView.swift
+++ C02-RPS/C02-RPS/ContentView.swift
@@ -8,9 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    let options = ["Rock", "Paper", "Scissor"]
+    let winChoices = ["Paper", "Scissor", "Rock"]
+    let loseChoices = ["Scissor", "Rock", "Paper"]
+    
+    @State private var question = Int.random(in: 0...2)
+    @State private var toWin = Bool.random()
+    
+    @State private var score = 0
+    
+    @State private var showNextButton = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text(options[question])
+            Text(toWin ? "Play to Win" : "Play to Lose")
+            
+            HStack {
+                ForEach(0..<3) { choice in
+                    Button {
+                        if toWin {
+                            if options[choice] == winChoices[question] {
+                                score += 10
+                            } else {
+                                score -= 10
+                            }
+                        } else {
+                            if options[choice] == loseChoices[question] {
+                                score += 10
+                            } else {
+                                score -= 10
+                            }
+                        }
+                        
+                        showNextButton = true
+                    } label: {
+                        Text("\(options[choice])")
+                    }
+                    .disabled(showNextButton)
+                }
+            }
+            
+            Text(score, format: .number)
+            
+            Button("Next") {
+                nextQuestion()
+                showNextButton = false
+            }
+            .disabled(!showNextButton)
+        }
+    }
+    
+    func nextQuestion() {
+        question = Int.random(in: 0...2)
+        toWin = Bool.random()
     }
 }
 
