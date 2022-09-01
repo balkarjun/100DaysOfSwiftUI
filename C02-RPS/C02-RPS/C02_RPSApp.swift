2a755a0e05c4a440d86b6c3a8b0c0ede87d72ef0
Arjun Balakrishnan <balkarjun@gmail.com>
2022-09-01 17:31:43 +0530

    add project

---
+++ C02-RPS/C02-RPS/C02_RPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  C02_RPSApp.swift
+//  C02-RPS
+//
+//  Created by Arjun B on 01/09/22.
+//
+
+import SwiftUI
+
+@main
+struct C02_RPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ C02-RPS/C02-RPS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  C02-RPS
+//
+//  Created by Arjun B on 01/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
