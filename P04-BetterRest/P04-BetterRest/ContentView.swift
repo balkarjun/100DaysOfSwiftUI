ba2a02d748e6bfc624fb9c24f46849b6d079ae75
Arjun Balakrishnan <balkarjun@gmail.com>
2022-09-07 15:54:31 +0530

    add new project

---
+++ P04-BetterRest/P04-BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P04-BetterRest
+//
+//  Created by Arjun B on 07/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ P04-BetterRest/P04-BetterRest/P04_BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  P04_BetterRestApp.swift
+//  P04-BetterRest
+//
+//  Created by Arjun B on 07/09/22.
+//
+
+import SwiftUI
+
+@main
+struct P04_BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
