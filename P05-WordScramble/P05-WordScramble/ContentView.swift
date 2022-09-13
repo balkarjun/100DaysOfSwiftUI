8ebe95c7571b964e4454921c7256e2c6f59c7e87
Arjun Balakrishnan <balkarjun@gmail.com>
2022-09-13 17:10:52 +0530

    add new project

---
+++ P05-WordScramble/P05-WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P05-WordScramble
+//
+//  Created by Arjun B on 13/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ P05-WordScramble/P05-WordScramble/P05_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  P05_WordScrambleApp.swift
+//  P05-WordScramble
+//
+//  Created by Arjun B on 13/09/22.
+//
+
+import SwiftUI
+
+@main
+struct P05_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
