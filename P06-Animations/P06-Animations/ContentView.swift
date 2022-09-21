d5714ab70a6cf0c4fdbaa7ba6aba2c770adcbf99
Arjun Balakrishnan <balkarjun@gmail.com>
2022-09-21 19:22:40 +0530

    add new project

---
+++ P06-Animations/P06-Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P06-Animations
+//
+//  Created by Arjun B on 21/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ P06-Animations/P06-Animations/P06_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  P06_AnimationsApp.swift
+//  P06-Animations
+//
+//  Created by Arjun B on 21/09/22.
+//
+
+import SwiftUI
+
+@main
+struct P06_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
