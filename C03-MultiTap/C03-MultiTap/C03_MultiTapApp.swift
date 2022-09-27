d3433ae7a4c4bd7aeb92b9329fb777f64b8dcb5e
Arjun Balakrishnan <balkarjun@gmail.com>
2022-09-27 22:30:11 +0530

    add new project

---
+++ C03-MultiTap/C03-MultiTap/C03_MultiTapApp.swift
@@ -0,0 +1,17 @@
+//
+//  C03_MultiTapApp.swift
+//  C03-MultiTap
+//
+//  Created by Arjun B on 27/09/22.
+//
+
+import SwiftUI
+
+@main
+struct C03_MultiTapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
