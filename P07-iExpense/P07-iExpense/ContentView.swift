e850bff1f0ca1047e607f1b62ad447cea3c4ce60
Arjun Balakrishnan <balkarjun@gmail.com>
2022-10-04 17:25:34 +0530

    add new project

---
+++ P07-iExpense/P07-iExpense/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  P07-iExpense
+//
+//  Created by Arjun B on 04/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ P07-iExpense/P07-iExpense/P07_iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  P07_iExpenseApp.swift
+//  P07-iExpense
+//
+//  Created by Arjun B on 04/10/22.
+//
+
+import SwiftUI
+
+@main
+struct P07_iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
