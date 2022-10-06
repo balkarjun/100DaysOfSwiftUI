e44d12d8a35c9ba0d1fc8e5d8bae5f15095c8db4
Arjun Balakrishnan <balkarjun@gmail.com>
2022-10-06 18:40:28 +0530

    add new project

---
+++ P08-Moonshot/P08-Moonshot/P08_MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  P08_MoonshotApp.swift
+//  P08-Moonshot
+//
+//  Created by Arjun B on 06/10/22.
+//
+
+import SwiftUI
+
+@main
+struct P08_MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
