bd0f125b24799ad298ef0c9ae66e64150004c25a
Arjun Balakrishnan <balkarjun@gmail.com>
2022-10-06 20:57:41 +0530

    update

--- P08-Moonshot/P08-Moonshot/ContentView.swift
+++ P08-Moonshot/P08-Moonshot/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
