639f4cc3a57f8b8106caf9e1e18a25ded01f8200
Arjun Balakrishnan <balkarjun@gmail.com>
2022-10-22 16:49:06 +0530

    update

--- P09-Drawing/P09-Drawing/ContentView.swift
+++ P09-Drawing/P09-Drawing/ContentView.swift
@@ -7,13 +7,49 @@
 
 import SwiftUI
 
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+
+        return path
+    }
+}
+
+struct Arc: Shape {
+    let startAngle: Angle
+    let endAngle: Angle
+    let clockwise: Bool
+    
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStartAngle = startAngle - rotationAdjustment
+        let modifiedEndAngle = endAngle - rotationAdjustment
+        
+        var path = Path()
+        
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifiedStartAngle, endAngle: modifiedEndAngle, clockwise: !clockwise)
+        
+        return path
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Triangle()
+                .stroke(.red.gradient, style: StrokeStyle(lineWidth: 16, lineCap: .round, lineJoin: .round))
+                .frame(width: 240, height: 200)
+            
+            Spacer()
+            
+            Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
+                .stroke(.blue.gradient, style: StrokeStyle(lineWidth: 16, lineCap: .round, lineJoin: .round))
+                .frame(width: 300, height: 300)
         }
         .padding()
     }
