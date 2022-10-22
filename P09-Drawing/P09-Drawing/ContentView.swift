53cc4eeb2686a6ea216a6f40c49a6a06b4367c82
Arjun Balakrishnan <balkarjun@gmail.com>
2022-10-23 01:36:36 +0530

    update

--- P09-Drawing/P09-Drawing/ContentView.swift
+++ P09-Drawing/P09-Drawing/ContentView.swift
@@ -47,7 +47,7 @@ struct Arc: InsettableShape {
     }
 }
 
-struct ContentView: View {
+struct Day43: View {
     var body: some View {
         VStack {
             ZStack {
@@ -64,6 +64,102 @@ struct ContentView: View {
                     .frame(width: 50, height: 40)
             }
         }
+    }
+}
+
+struct Flower: Shape {
+    var petalOffset = -20.0
+    var petalWidth = 100.0
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width/2))
+
+        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi/8) {
+            let rotation = CGAffineTransform(rotationAngle: number)
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width/2, y: rect.height/2))
+            
+            let rotatedPetal = originalPetal.applying(position)
+            path.addPath(rotatedPetal)
+        }
+        return path
+    }
+}
+
+struct Day44Part1: View {
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+    
+    var body: some View {
+        VStack {
+            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                .fill(.orange, style: FillStyle(eoFill: true))
+                .padding()
+            
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.horizontal, .bottom])
+            
+            Text("Width")
+            Slider(value: $petalWidth, in: -40...100)
+                .padding([.horizontal, .bottom])
+        }
+    }
+}
+
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                                    color(for: value, brightness: 1),
+                                    color(for: value, brightness: 0.5)
+                                ]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
+                        lineWidth: 2
+                    )
+            }
+        }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
+struct Day44Part2: View {
+    @State private var colorCycle = 0.0
+    
+    var body: some View {
+        VStack {
+            ColorCyclingCircle(amount: colorCycle)
+                .frame(width: 300, height: 300)
+            
+            Slider(value: $colorCycle)
+        }
+    }
+    
+}
+
+struct ContentView: View {
+    var body: some View {
+        Day44Part2()
     }
 }
 
