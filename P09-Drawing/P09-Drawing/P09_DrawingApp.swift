552d55ce20303eae303a0ae55e37fd8b14300063
Arjun Balakrishnan <balkarjun@gmail.com>
2022-10-22 16:26:24 +0530

    add new project

---
+++ P09-Drawing/P09-Drawing/P09_DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  P09_DrawingApp.swift
+//  P09-Drawing
+//
+//  Created by Arjun B on 22/10/22.
+//
+
+import SwiftUI
+
+@main
+struct P09_DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
