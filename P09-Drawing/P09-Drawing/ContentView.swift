89f26923e5eacb1391ad9fbd0230668f690e9557
Arjun Balakrishnan <balkarjun@gmail.com>
2022-10-23 13:12:58 +0530

    update

--- P09-Drawing/P09-Drawing/ContentView.swift
+++ P09-Drawing/P09-Drawing/ContentView.swift
@@ -157,9 +157,43 @@ struct Day44Part2: View {
     
 }
 
+struct Trapezoid: Shape {
+    var insetAmount: Double
+    
+    var animatableData: Double {
+        get { insetAmount }
+        set { insetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        
+        return path
+    }
+}
+
+struct Day44Part3: View {
+    @State private var insetAmount = 50.0
+    var body: some View {
+        Trapezoid(insetAmount: insetAmount)
+            .frame(width: 200, height: 100)
+            .onTapGesture {
+                withAnimation {
+                    insetAmount = Double.random(in: 10...90)
+                }
+            }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Day44Part2()
+        Day44Part3()
     }
 }
 
