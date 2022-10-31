d4abcf03cdde496d0ded08bf6611bad7083ab1f1
Arjun Balakrishnan <balkarjun@gmail.com>
2022-10-31 17:21:14 +0530

    add new project

---
+++ P11-Bookworm/P11-Bookworm/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  P11-Bookworm
+//
+//  Created by Arjun B on 31/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ P11-Bookworm/P11-Bookworm/P11_BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  P11_BookwormApp.swift
+//  P11-Bookworm
+//
+//  Created by Arjun B on 31/10/22.
+//
+
+import SwiftUI
+
+@main
+struct P11_BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
